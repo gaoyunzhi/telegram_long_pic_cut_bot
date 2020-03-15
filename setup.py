9a441864836cd1610d21c39040d5060918225c86
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-15 05:40:28 -0700

    commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-EXE_FILE = 'export'
+EXE_FILE = 'pic_bot'
 
 def kill():
 	os.system("ps aux | grep ython | grep %s | awk '{print $2}' | xargs kill -9" % EXE_FILE)
@@ -24,14 +24,6 @@ def setup(arg = ''):
 		from telegram.ext import Updater, MessageHandler, Filters
 	except:
 		os.system('sudo pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
-
-	try:
-		import yaml
-		with open('TELEGRAPH_TOKENS') as f:
-			TELEGRAPH_TOKENS = yaml.load(f, Loader=yaml.FullLoader)
-	except:
-		with open('TELEGRAPH_TOKENS', 'w') as f:
-			f.write(yaml.dump({}, sort_keys=True, indent=2))
 
 	kill()
 
