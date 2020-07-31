55f714cfc91a2bc07db705f781f463ebf2276796
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-31 13:29:05 -0700

    commit

--- pic_bot.py
+++ pic_bot.py
@@ -28,12 +28,14 @@ def cut(update, context):
 	result.cap = msg.caption_markdown or msg.text_markdown or ''
 	result.imgs = [file_path]
 
+	tmp = msg.reply_text('pic sending')
 	try:
 		r = album_sender.send_v2(msg.chat, result, send_all=True, sleep=5)
 	except:
 		return
 	if len(r) == 1 and file:
 		r.delete()
+	tmp.delete()
 
 tele.dispatcher.add_handler(MessageHandler(Filters.all, cut))
 
